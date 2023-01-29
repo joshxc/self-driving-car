611d60cefc06f166658652d940579bc5e156fed8
Josh Taylor <josht@duck.com>
2023-01-30 01:42:31 +1000

    Add code to find intersection of rays and road borders

--- sensor.js
+++ sensor.js
@@ -18,7 +18,28 @@ class Sensor {
   }
 
   #getReading(ray, roadBorders) {
+    let touches = [];
 
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBorders[i][0],
+        roadBorders[i][1]
+      );
+      if (touch) {
+        touches.push(touch);
+      }
+    }
+
+    if (touches.length == 0) {
+      return null;
+    } else {
+      const offsets = touches.map((el) => el.offset);
+      const minOffset = Math.min(...offsets);
+
+      return touches.find((el) => el.offset == minOffset);
+    }
   }
 
   // private method
@@ -47,12 +68,22 @@ class Sensor {
 
   draw(ctx) {
     for (let i = 0; i < this.rayCount; i++) {
+      let end = this.rays[i][1];
+      if (this.readings[i]) end = this.readings[i];
       ctx.beginPath();
       ctx.lineWidth = 2;
       ctx.strokeStyle = 'yellow';
 
       ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y); // move to start position of ray i
-      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y); // line to end position of ray i
+      ctx.lineTo(end.x, end.y); // line to end position of ray i
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = 'black';
+
+      ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y); // move to start position of ray i
+      ctx.lineTo(end.x, end.y); // line to end position of ray i
       ctx.stroke();
     }
   }
