ea17d5680f89ccb3b9e503488eaf5e916d1d7397
Josh Taylor <josht@duck.com>
2023-01-31 17:30:01 +1000

    Add functionality to save or discard the "best car" to and from local storage

--- app.js
+++ app.js
@@ -13,9 +13,24 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'AI');
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+// set the best brain to the best car
+if (localStorage.getItem('bestBrain')) {
+  bestCar.brain = JSON.parse(localStorage.getItem('bestBrain'));
+}
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'BOT', 2)];
 
 animate();
+
+// Save the 'best car' to local storage
+function save() {
+  localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain));
+}
+
+// Discard the 'best car' - if a better car exists
+function discard() {
+  localStorage.removeItem('bestBrain');
+}
 
 function generateCars(N) {
   const cars = [];
@@ -39,9 +54,7 @@ function animate(time) {
 
   // define best car (i.e. the car that survives the longest)
   // find the car with the minimum y value out of all cars y values
-  const bestCar = cars.find(
-    (car) => car.y == Math.min(...cars.map((car) => car.y))
-  );
+  bestCar = cars.find((car) => car.y == Math.min(...cars.map((car) => car.y)));
 
   // resizing the canvas here will clear existing car positions and only leave 1 box visible
   carCanvas.height = window.innerHeight;
