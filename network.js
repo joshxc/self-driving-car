9372fa77a049f1f6ff5b1fa09b52765307e3fa74
Josh Taylor <josht@duck.com>
2023-02-02 17:42:21 +1000

    Add a mutate method in network.js and implement it in app.js

--- network.js
+++ network.js
@@ -13,6 +13,26 @@ class NeuralNetwork {
     }
     return outputs;
   }
+
+  // mutate the previously saved network by an amount
+  // 0 is no modification
+  // 1 is a completely new network
+  static mutate(network, amount = 1) {
+    network.levels.forEach((level) => {
+      for (let i = 0; i < level.biases.length; i++) {
+        level.biases[i] = lerp(level.biases[i], Math.random() * 2 - 1, amount);
+      }
+      for (let i = 0; i < level.weights.length; i++) {
+        for (let j = 0; j < level.weights[i].length; j++) {
+          level.weights[i][j] = lerp(
+            level.weights[i][j],
+            Math.random() * 2 - 1,
+            amount
+          );
+        }
+      }
+    });
+  }
 }
 
 class Level {
